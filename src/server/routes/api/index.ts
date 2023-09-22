339ca4b0dd409336e94cb30988703d40815943d2
Jesse Cammarano <jcamma93@gmail.com>
2023-09-22 13:27:08 -0400

    routes finished/running postman tests

--- src/server/routes/api/index.ts
+++ src/server/routes/api/index.ts
@@ -1,12 +1,13 @@
 import * as express from 'express';
 import categoriesRouter from './categories'
 import pizzaRouter from './pizza';
+import booksRouter from './books';
 
 const router = express.Router();
 
 router.use('/categories', categoriesRouter);
 router.use('/pizza', pizzaRouter);
-
+router.use('/books', booksRouter);
 
 
 export default router;
